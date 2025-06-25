2583c4d2762f78405d2b77dc234006de81c773bf
Prince Agyemang <princeagyemang094@gmail.com>
2025-06-25 19:01:49 +0000

    This iss not the original typography. Montserrat font is what you propos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,6 @@ export default {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        body: ['Montserrat', 'sans-serif'],
-        headline: ['Montserrat', 'sans-serif'],
-        code: ['monospace'],
-      },
       colors: {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
